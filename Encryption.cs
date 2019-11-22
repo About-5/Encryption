f69370d3d09b88e6a1d99caa60a8bd38d2be4b0b
About-5 <37988112+About-5@users.noreply.github.com>
2019-11-22 10:43:11 -0800

    Add files via upload
    vigenere & fix caeser

--- Encryption.cs
+++ Encryption.cs
@@ -6,8 +6,7 @@ namespace TestingCS
 {
     public class Encryption
     {
-        public string encryptCaeser(string plainText, int shift)
-        {
+        public string encryptCaeser(string plainText, int shift) {
             StringBuilder cipherText = new StringBuilder("", plainText.Length);
             plainText = plainText.ToUpper();
 
@@ -27,14 +26,13 @@ namespace TestingCS
             return cipherText.ToString();
         }
 
-        public string decryptCaeser(string cipherText, int shift)
-        {
+        public string decryptCaeser(string cipherText, int shift) {
             StringBuilder plainText = new StringBuilder("", cipherText.Length);
             cipherText = cipherText.ToUpper();
 
             for (int i = 0; i < cipherText.Length; i++) {
                 int ascii = (int)cipherText[i] - shift;
-                if (ascii < 60) {
+                if (ascii < 39) {
                     plainText.Insert(i, cipherText[i]);
                     
                 } else if (ascii > 64) {
@@ -77,11 +75,15 @@ namespace TestingCS
             return cipherText.ToString();
         }
 
-        public string substitutionDecrypt(string cipherText, string code)
-        {
+        public string substitutionDecrypt(string cipherText, string code) {
             StringBuilder plainText = new StringBuilder("", cipherText.Length);
             cipherText = cipherText.ToUpper();
             code = code.ToUpper();
+            if (code.Length != 26) {
+                Console.WriteLine("Bad encoding string \n");
+                return null;
+            }
+
             Dictionary<int, int> list = new Dictionary<int, int>()
             {
                 {32, -33}
@@ -102,10 +104,60 @@ namespace TestingCS
             return plainText.ToString();    
         }
 
+        public string vigenereEncrypt(string plainText, string keyword) {
+            StringBuilder cipherText = new StringBuilder("", plainText.Length);
+            plainText = plainText.ToUpper();
+            int count = 0;
+            if(keyword.Length < 1) {
+                Console.WriteLine("Bad keyword string \n");
+                return null;
+            }
+
+            for(int i = 0; i < plainText.Length; i++) {
+                int index = count % keyword.Length;
+                int ascii = (int)plainText[i] + ((int)keyword[index] - 64);
+                if(ascii < 65) {
+                    cipherText.Insert(i, plainText[i]);
+                } else if(ascii > 90) {
+                    ascii = ascii - 26;
+                    cipherText.Insert(i, (char)ascii);
+                } else {
+                    cipherText.Insert(i, (char)ascii);
+                }
+                count++;
+            }
+            return cipherText.ToString();
+        }
+
+        public string vigenereDecrypt(string CipherText, string keyword) {
+            StringBuilder plainText = new StringBuilder("", CipherText.Length);
+            CipherText = CipherText.ToUpper();
+            int count = 0;
+            if(keyword.Length < 1) {
+                Console.WriteLine("Bad keyword string \n");
+                return null;
+            }
+
+            for(int i = 0; i < CipherText.Length; i++) {
+                int index = count % keyword.Length;
+                int ascii = (int)CipherText[i] - ((int)keyword[index] - 64);
+                if(ascii < 39) {
+                    plainText.Insert(i, CipherText[i]);
+                } else if(ascii > 64) {
+                    plainText.Insert(i, (char)ascii);
+                } else {
+                    ascii = ascii + 26;
+                    plainText.Insert(i, (char)ascii);
+                }
+                count++;
+            }
+            return plainText.ToString();
+        }
+
         public static void Main(string[] args) {
             string plainText = "How is your day today";
             Encryption encrypt = new Encryption();
-            int caeserShift = 5;
+            int caeserShift = 10;
             string cipherText = encrypt.encryptCaeser(plainText, caeserShift);
             string decryptedText = encrypt.decryptCaeser(cipherText, caeserShift);
             Console.WriteLine("Caeser cipher: \n{0} \n{1} \n{2}", plainText, cipherText, decryptedText);
@@ -113,6 +165,10 @@ namespace TestingCS
             cipherText = encrypt.substitutionEncrypt(plainText, code);
             decryptedText = encrypt.substitutionDecrypt(cipherText, code);
             Console.WriteLine("Substitution: \n{0} \n{1} \n{2}", plainText, cipherText, decryptedText);
+            string keyword = "DOG";
+            cipherText = encrypt.vigenereEncrypt(plainText, keyword);
+            decryptedText = encrypt.vigenereDecrypt(cipherText, keyword);
+            Console.WriteLine("Vigenere cipher: \n{0} \n{1} \n{2}", plainText, cipherText, decryptedText);
         }
     }
 }

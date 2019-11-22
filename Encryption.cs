06dcff1b3ba4596b0637ea012428777198054224
About-5 <37988112+About-5@users.noreply.github.com>
2019-11-22 06:36:16 -0800

    Add files via upload
    substitution decryption

--- Encryption.cs
+++ Encryption.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace TestingCS
 {
@@ -58,37 +59,28 @@ namespace TestingCS
             return plainText.ToString();
         }
 
-        public string substitutionEncrypt(string plainText, string code)
-        {
+        public string substitutionEncrypt(string plainText, string code) {
             StringBuilder cipherText = new StringBuilder("", plainText.Length);
             plainText = plainText.ToUpper();
 
-            if (code.Length != 26)
-            {
+            if (code.Length != 26) {
                 Console.WriteLine("Bad encoding string \n");
                 return null;
             }
 
-            for (int i = 0; i < plainText.Length; i++)
-            {
+            for (int i = 0; i < plainText.Length; i++) {
                 int index = (int)plainText[i] - 65;
 
-                if(index >= 0)
-                {
+                if(index >= 0) {
                     int ascii = (int)code[index];
                     
-                    if (ascii < 91)
-                    {
+                    if (ascii < 91) {
                         cipherText.Insert(i, (char)ascii);
-                    }
-                    else 
-                    {
+                    } else {
                         ascii = ascii - 26;
                         cipherText.Insert(i, (char)ascii);
                     }
-                }
-                else
-                {
+                } else {
                     cipherText.Insert(i, plainText[i]);
                 }
             }
@@ -100,6 +92,11 @@ namespace TestingCS
         {
             StringBuilder plainText = new StringBuilder("", cipherText.Length);
             cipherText = cipherText.ToUpper();
+            code = code.ToUpper();
+            Dictionary<int, int> list = new Dictionary<int, int>();
+            for(int i = 0; i < code.Length; i++) {
+                list.Add(code[i], i);
+            } 
 
             if(code.Length != 26)
             {
@@ -109,26 +106,8 @@ namespace TestingCS
 
             for(int i = 0; i < cipherText.Length; i++)
             {
-                int index = (int)cipherText[i] - 65;
-
-                if (index >= 0)
-                {
-                    int ascii = (int)code[index];
-
-                    if (ascii > 64)
-                    {
-                        plainText.Insert(i, (char)ascii);
-                    }
-                    else
-                    {
-                        ascii = ascii + 26;
-                        plainText.Insert(i, (char)ascii);
-                    }
-                } 
-                else
-                {
-                    plainText.Insert(i, cipherText[i]);
-                }
+                plainText.Insert(i, (char)(list[cipherText[i]] + 65));
+                
             }
 
             return plainText.ToString();    
@@ -136,7 +115,7 @@ namespace TestingCS
 
         public static void Main(string[] args)
         {
-            string plainText = "How is your day today";
+            string plainText = "animal";
             Encryption encrypt = new Encryption();
             int caeserShift = 5;
             string cipherText = encrypt.encryptCaeser(plainText, caeserShift);
@@ -144,7 +123,7 @@ namespace TestingCS
             Console.WriteLine("Caeser cipher: \n{0} \n{1} \n{2}", plainText, cipherText, decryptedText);
             string code = "ZXCVBNMASDFGHJKLPOIUYTREWQ";
             cipherText = encrypt.substitutionEncrypt(plainText, code);
-            decryptedText = encrypt.substitutionEncrypt(cipherText, code);
+            decryptedText = encrypt.substitutionDecrypt(cipherText, code);
             Console.WriteLine("Substitution: \n{0} \n{1} \n{2}", plainText, cipherText, decryptedText);
         }
     }
